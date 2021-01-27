4f1095fcd3b071e208eb535f2552fd261d0801ed
Hongrun Wu <ms.wuhr@gmail.com>
2021-01-28 02:52:28 +0800

    Add files via upload

---
+++ GRCNN/lib/utils.py
@@ -0,0 +1,218 @@
+import os
+import numpy as np
+import torch
+import torch.utils.data
+from sklearn import metrics
+from .metrics import masked_mape_np
+from scipy.sparse.linalg import eigs
+
+
+def re_normalization(x, mean, std):
+    x = x * std + mean
+    return x
+
+
+def max_min_normalization(x, _max, _min):
+    x = 1. * (x - _min)/(_max - _min)
+    x = x * 2. - 1.
+    return x
+
+
+def re_max_min_normalization(x, _max, _min):
+    x = (x + 1.) / 2.
+    x = 1. * x * (_max - _min) + _min
+    return x
+
+
+
+
+def load_graphdata_channel1(graph_signal_matrix_filename, num_of_hours, num_of_days, num_of_weeks, DEVICE, batch_size, shuffle=True):
+
+    file = os.path.basename(graph_signal_matrix_filename).split('.')[0]
+
+    dirpath = os.path.dirname(graph_signal_matrix_filename)
+
+    filename = os.path.join(dirpath,
+                            file + '_r' + str(num_of_hours) + '_d' + str(num_of_days) + '_w' + str(num_of_weeks)) +'_astcgn'
+
+    print('load file:', filename)
+
+    file_data = np.load(filename + '.npz')
+    train_x = file_data['train_x']  # (10181, 307, 3, 12)
+    train_x = train_x[:, :, 0:1, :]
+    train_target = file_data['train_target']  # (10181, 307, 12)
+
+    val_x = file_data['val_x']
+    val_x = val_x[:, :, 0:1, :]
+    val_target = file_data['val_target']
+
+    test_x = file_data['test_x']
+    test_x = test_x[:, :, 0:1, :]
+    test_target = file_data['test_target']
+
+    mean = file_data['mean'][:, :, 0:1, :]  # (1, 1, 3, 1)
+    std = file_data['std'][:, :, 0:1, :]  # (1, 1, 3, 1)
+
+    # ------- train_loader -------
+    train_x_tensor = torch.from_numpy(train_x).type(torch.FloatTensor).to(DEVICE)  # (B, N, F, T)
+    train_target_tensor = torch.from_numpy(train_target).type(torch.FloatTensor).to(DEVICE)  # (B, N, T)
+
+    train_dataset = torch.utils.data.TensorDataset(train_x_tensor, train_target_tensor)
+
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle)
+
+    # ------- val_loader -------
+    val_x_tensor = torch.from_numpy(val_x).type(torch.FloatTensor).to(DEVICE)  # (B, N, F, T)
+    val_target_tensor = torch.from_numpy(val_target).type(torch.FloatTensor).to(DEVICE)  # (B, N, T)
+
+    val_dataset = torch.utils.data.TensorDataset(val_x_tensor, val_target_tensor)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+    # ------- test_loader -------
+    test_x_tensor = torch.from_numpy(test_x).type(torch.FloatTensor).to(DEVICE)  # (B, N, F, T)
+    test_target_tensor = torch.from_numpy(test_target).type(torch.FloatTensor).to(DEVICE)  # (B, N, T)
+
+    test_dataset = torch.utils.data.TensorDataset(test_x_tensor, test_target_tensor)
+
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    # print
+    print('train:', train_x_tensor.size(), train_target_tensor.size())
+    print('val:', val_x_tensor.size(), val_target_tensor.size())
+    print('test:', test_x_tensor.size(), test_target_tensor.size())
+
+    return train_loader, train_target_tensor, val_loader, val_target_tensor, test_loader, test_target_tensor, mean, std
+
+
+def compute_val_loss_grcnn(net, val_loader, criterion, sw, epoch, limit=None):
+
+    net.train(False)  # ensure dropout layers are in evaluation mode
+
+    with torch.no_grad():
+
+        val_loader_length = len(val_loader)  # nb of batch
+
+        tmp = []
+
+        for batch_index, batch_data in enumerate(val_loader):
+            encoder_inputs, labels = batch_data
+            outputs = net(encoder_inputs)
+            loss = criterion(outputs, labels)  # loss
+            tmp.append(loss.item())
+            if batch_index % 100 == 0:
+                print('validation batch %s / %s, loss: %.2f' % (batch_index + 1, val_loader_length, loss.item()))
+            if (limit is not None) and batch_index >= limit:
+                break
+
+        validation_loss = sum(tmp) / len(tmp)
+        sw.add_scalar('validation_loss', validation_loss, epoch)
+    return validation_loss
+
+
+def evaluate_on_test_grcnn(net, test_loader, test_target_tensor, sw, epoch, _mean, _std):
+
+    net.train(False)  # ensure dropout layers are in test mode
+
+    with torch.no_grad():
+
+        test_loader_length = len(test_loader)
+
+        test_target_tensor = test_target_tensor.cpu().numpy()
+
+        prediction = []  # 存储所有batch的output
+
+        for batch_index, batch_data in enumerate(test_loader):
+
+            encoder_inputs, labels = batch_data
+
+            outputs = net(encoder_inputs)
+
+            prediction.append(outputs.detach().cpu().numpy())
+
+            if batch_index % 100 == 0:
+                print('predicting testing set batch %s / %s' % (batch_index + 1, test_loader_length))
+
+        prediction = np.concatenate(prediction, 0)  # (batch, T', 1)
+        prediction_length = prediction.shape[2]
+
+        for i in range(prediction_length):
+            assert test_target_tensor.shape[0] == prediction.shape[0]
+            print('current epoch: %s, predict %s points' % (epoch, i))
+            cl = torch.nn.CrossEntropyLoss(test_target_tensor[:, :, i], prediction[:, :, i])
+            print('cl: %.2f' % (cl))
+            print()
+            if sw:
+                sw.add_scalar('cl%s_points' % (i), cl, epoch)
+
+
+def predict_and_save_results_grcnn(net, data_loader, data_target_tensor, global_step, params_path, type, _mean, _std):
+
+    net.train(False)  # ensure dropout layers are in test mode
+
+    with torch.no_grad():
+
+        data_target_tensor = data_target_tensor.cpu().numpy()
+
+        loader_length = len(data_loader)  # nb of batch
+
+        prediction = []  # 存储所有batch的output
+
+        input = []  # 存储所有batch的input
+
+        for batch_index, batch_data in enumerate(data_loader):
+
+            encoder_inputs, labels = batch_data
+
+            input.append(encoder_inputs[:, :, 0:1].cpu().numpy())  # (batch, T', 1)
+
+            outputs = net(encoder_inputs)
+
+            prediction.append(outputs.detach().cpu().numpy())
+
+            if batch_index % 100 == 0:
+                print('predicting data set batch %s / %s' % (batch_index + 1, loader_length))
+
+        input = np.concatenate(input, 0)
+
+        input = re_normalization(input, _mean, _std)
+
+        prediction = np.concatenate(prediction, 0)  # (batch, T', 1)
+
+        print('input:', input.shape)
+        print('prediction:', prediction.shape)
+        print('data_target_tensor:', data_target_tensor.shape)
+        output_filename = os.path.join(params_path, 'output_epoch_%s_%s' % (global_step, type))
+        np.savez(output_filename, input=input, prediction=prediction, data_target_tensor=data_target_tensor)
+
+        # 计算误差
+        excel_list = []
+        prediction_length = prediction.shape[2]
+
+        for i in range(prediction_length):
+            assert data_target_tensor.shape[0] == prediction.shape[0]
+            print('current epoch: %s, predict %s points' % (global_step, i))
+            acc = metrics.accuracy_score(data_target_tensor[:, :, i], prediction[:, :, i])
+            pre = metrics.precision_score(data_target_tensor[:, :, i], prediction[:, :, i])
+            rec = metrics.recall_score(data_target_tensor[:, :, i], prediction[:, :, i])
+            f1 = metrics.f1_score(data_target_tensor[:, :, i], prediction[:, :, i])
+            print('acc: %.2f' % (acc)
+            print('pre: %.2f' % (rec))
+            print('rec: %.2f' % (rec))
+            print('f1: %.2f' % (f1))
+            excel_list.extend([acc, pre, rec,f1])
+
+        # print overall results
+        acc = metrics.accuracy_score(data_target_tensor.reshape(-1, 1), prediction.reshape(-1, 1))
+        pre = metrics.precision_score(data_target_tensor.reshape(-1, 1), prediction.reshape(-1, 1))
+        rec = metrics.recall_score(data_target_tensor.reshape(-1, 1), prediction.reshape(-1, 1), 1)
+        f1 = metrics.precision_score(data_target_tensor.reshape(-1, 1), prediction.reshape(-1, 1))
+
+        print('all acc: %.2f' % (acc))
+        print('all pre: %.2f' % (pre))
+        print('all rec: %.2f' % (rec))
+        print('all f1: %.2f' % (f1))
+        excel_list.extend([acc, pre, rec, f1])
+        print(excel_list)
+
+
